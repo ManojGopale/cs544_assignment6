40fe9880007eb9ebd30a38fe4e63d77799663176
Gopale <manojgopale@oscr-mz156.catnet.arizona.edu>
2017-10-30 01:05:38 -0700

    Table for the SATM, SATV, ACT and GPA for students

---
+++ table.js
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8"/>
+		<title> Assignment 6 - Manoj Gopale </title>
+		<link href="./style/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+<!--		<script src="https://cscheid.net/courses/fal17/csc544/lecture-extras/lecture3/data.js"></script> -->
+		<div id="scatterplot_1">
+		</div>
+		<table>
+			<thead>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+				</tr>
+				<tr>
+					<td> John </td>
+					<td> Doe </td>
+				</tr>
+				<tr>
+					<td> Jane </td>
+					<td> Doe </td>
+				</tr>
+			</thead>
+		</table>
+
+		<table>
+			<thead>
+				<tr>
+					<th> SATM </th>
+					<th> SATM </th>
+					<th> ACT </th>
+					<th> GPA </th>
+				</tr>
+				<tr>
+					<td> 500 </td>
+					<td> 498 </td>
+					<td> 34 </td>
+					<td> 3.5 </td>
+				</tr>
+			</thead>
+		</table>
+		<script src="https://d3js.org/d3.v4.min.js"></script>
+		<script src="https://cscheid.net/courses/fal17/csc544/assignment_2/scores.js"></script>
+		<script src="https://cscheid.net/courses/fal17/csc544/assignment_3/ukDriverFatalities.js"></script>
+		<script src="https://d3js.org/d3-axis.v1.min.js"></script>
+		<script src="https://d3js.org/d3-color.v1.min.js"></script>
+		<script src="https://d3js.org/d3-interpolate.v1.min.js"></script>
+		<script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
+		<script src="http://colorbrewer2.org/export/colorbrewer.js"></script>
+		<!-- <script src="./scripts/hw3_trial.js"></script> -->
+		<!-- <script src="./table.js"></script> -->
+		<script src="./table.js"></script>
+	</body>
+</html>
